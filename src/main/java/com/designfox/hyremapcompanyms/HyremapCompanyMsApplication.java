782f4f8b4160ff8d1e5958914dfba57a9b84c6ae
Vikki <vikas@hood.live>
2024-04-12 11:43:27 +0530

    Blank spring boot application

---
+++ src/main/java/com/designfox/hyremapcompanyms/HyremapCompanyMsApplication.java
@@ -0,0 +1,13 @@
+package com.designfox.hyremapcompanyms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyremapCompanyMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyremapCompanyMsApplication.class, args);
+	}
+
+}
